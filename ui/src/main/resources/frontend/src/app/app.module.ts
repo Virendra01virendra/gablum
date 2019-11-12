395e6c3cb38bea5ee6d7e28495e59cd41be8a71c
ananyo sen <ananyo.sen@boeing.com>
2019-11-12 10:46:17 +0530

    add boilerplate
    add the boilerplate code for java microservices, including the ci and
    docker files

---
+++ ui/src/main/resources/frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppMaterialModule } from './app-material/app-material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AppMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
