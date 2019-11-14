e7a64615ad90a74fe3f4afcf470e116322c813c7
ananyo sen <ananyo.sen@boeing.com>
2019-11-14 15:34:54 +0530

    test for angular communicator-service

--- gablum-webapp/src/main/resources/frontend/src/app/services/communicator.service.spec.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/services/communicator.service.spec.ts
@@ -3,10 +3,20 @@ import { TestBed } from '@angular/core/testing';
 import { CommunicatorService } from './communicator.service';
 
 describe('CommunicatorService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: CommunicatorService = TestBed.get(CommunicatorService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: CommunicatorService = TestBed.get(CommunicatorService);
+        expect(service).toBeTruthy();
+    });
+
+    it('should be able to communicate', () => {
+        const service: CommunicatorService = TestBed.get(CommunicatorService);
+        const messageData = {data: 'some_data'};
+
+        service.postMessage(this, 'dest', messageData);
+        service.getMessages().subscribe((msg) => {
+            expect(msg.data).toEqual(messageData);
+        });
+    });
 });
