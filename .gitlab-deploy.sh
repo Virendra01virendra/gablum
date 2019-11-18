5134a04cb33f68147b503ef2179eeb71ef819fc9
Darshan.Shah <darshan.shah@stackroute.in>
2019-11-18 14:43:08 +0530

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -27,12 +27,11 @@ apt-get update && apt-get install -y openssh-client
 #  rm -rf /home/ubuntu/gablumplatform && \
 
 command="ls -ltr && \
-rm -rf /home/ubuntu/gablum && \
-rm -rf /home/ubuntu/gablumplatform && \
+ rm -rf /home/ubuntu/gablumplatform
  mkdir -p /home/ubuntu/gablumplatform && \
  cd /home/ubuntu/gablumplatform && \
  git clone https://${gituser}:${gittoken}@gitlab.stackroute.in/gablum/gablumplatform.git -b ${branch} && \
- cd /home/ubuntu/gablumplatform && \
+ cd /home/ubuntu/gablumplatform/gablumplatform && \
  ls -ltr && \
  echo 'Deploying the Gablum Application' && \
  docker-compose up --build -d --remove-orphans && \
