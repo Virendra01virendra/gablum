c56b23aefb6213fac139b2418dcecf9148cad184
Darshan.Shah <darshan.shah@stackroute.in>
2019-11-18 12:06:41 +0530

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -14,25 +14,25 @@ apt-get update && apt-get install -y openssh-client
 
 ## Rolling Update
 
-command="ls -ltr && \
- cd /home/devuser/gablum && \
- git pull origin ${branch} && \
- docker-compose up --build -d --remove-orphans && \
- echo 'DONE DEPLOYING'"
-
-# Complete Build
-
 # command="ls -ltr && \
-#  mkdir -p /home/devuser/gablum && \
-#  cd /home/devuser/gablum && \
-#  docker-compose -f docker-compose.yml down && \
-#  cd /home/devuser && \
-#  rm -rf /home/devuser/gablum && \
-#  git clone https://${gituser}:${gittoken}@gitlab.stackroute.in/gablum/gablumplatform.git -b ${branch} && \
-#  cd /home/devuser/gablum && \
-#  echo 'Deploying the Gablum Application' && \
+#  cd /home/devuser/gablumplatform && \
+#  git pull origin ${branch} && \
 #  docker-compose up --build -d --remove-orphans && \
 #  echo 'DONE DEPLOYING'"
+
+Complete Build
+
+command="ls -ltr && \
+ mkdir -p /home/devuser/gablumplatform && \
+ cd /home/devuser/gablumplatform && \
+ docker-compose -f docker-compose.yml down && \
+ cd /home/devuser && \
+ rm -rf /home/devuser/gablumplatform && \
+ git clone https://${gituser}:${gittoken}@gitlab.stackroute.in/gablum/gablumplatform.git -b ${branch} && \
+ cd /home/devuser/gablumplatform && \
+ echo 'Deploying the Gablum Application' && \
+ docker-compose up --build -d --remove-orphans && \
+ echo 'DONE DEPLOYING'"
 
 
 
