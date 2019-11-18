2946686fa358dc70aea85d3d6876460a393e7238
ananyo sen <ananyo.sen@boeing.com>
2019-11-18 11:56:11 +0530

    add tests for AuctionRepo
    testing using embedded-mongo on AuctionRepo

--- gablum-auction/auctions/src/main/java/com/gablum/auction/auctions/AuctionRepo.java
+++ gablum-auction/auctions/src/main/java/com/gablum/auction/auctions/AuctionRepo.java
@@ -3,5 +3,9 @@ package com.gablum.auction.auctions;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface AuctionRepo extends MongoRepository<Auction, ObjectId> {
+
+    public Optional<Auction> findByAuctionId(long auctionId);
 }
