980ab98cdc06bfc0e936c9d798fa89a4aa756787
ananyo sen <ananyo.sen@boeing.com>
2019-11-18 18:16:20 +0530

    fixing tests

--- gablum-webapp/src/main/resources/frontend/src/app/dashboard/dashboard/dashboard.component.spec.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/dashboard/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { AuctionsListComponent } from '../auctions-list/auctions-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,7 +10,8 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      imports: [HttpClientModule],
+      declarations: [ DashboardComponent, AuctionsListComponent]
     })
     .compileComponents();
   }));
