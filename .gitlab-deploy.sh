8ede9f42067ec97b494dafabec31b7806cf95bed
Darshan.Shah <darshan.shah@stackroute.in>
2019-11-18 10:19:01 +0530

    added cd stage for product

---
+++ .gitlab-deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -f
+
+server=$DEPLOY_SERVER
+user=$DEPLOY_USER
+branch=$DEPLOY_BRANCH
+gittoken=$DEPLOY_GITLAB_TOKEN
+gituser=$DEPLOY_GITLAB_USER
+
+echo "Deploying project on server ${server} as ${user} from branch ${branch}"
+
+apt-get update && apt-get install -y openssh-client
+
+## Rolling Update
+
+command="ls -ltr && \
+ cd /home/devuser/gablum && \
+ git pull origin ${branch} && \
+ docker-compose up --build -d --remove-orphans && \
+ echo 'DONE DEPLOYING'"
+
+# Complete Build
+
+# command="ls -ltr && \
+#  mkdir -p /home/devuser/gablum && \
+#  cd /home/devuser/gablum && \
+#  docker-compose -f docker-compose.yml down && \
+#  cd /home/devuser && \
+#  rm -rf /home/devuser/gablum && \
+#  git clone https://${gituser}:${gittoken}@gitlab.stackroute.in/gablum/gablumplatform.git -b ${branch} && \
+#  cd /home/devuser/gablum && \
+#  echo 'Deploying the Gablum Application' && \
+#  docker-compose up --build -d --remove-orphans && \
+#  echo 'DONE DEPLOYING'"
+
+
+
+echo "About to run the command: " $command
+
+ssh $user@$server $command
