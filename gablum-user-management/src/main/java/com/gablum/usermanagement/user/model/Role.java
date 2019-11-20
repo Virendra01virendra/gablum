bda476fc0ff3962ecb7801364b0fdf24e1eca470
Abhishek <er.abhisheksingh2764@gmail.com>
2019-11-20 15:33:15 +0530

    The Jwt feature is working. The feature is yet to be aligned according to the micro-services.

---
+++ gablum-user-management/src/main/java/com/gablum/usermanagement/user/model/Role.java
@@ -0,0 +1,41 @@
+package com.gablum.usermanagement.user.model;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@AllArgsConstructor
+public class Role {
+    private Integer id;
+    private String role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
---
+++ gablum-user-management/src/main/java/com/gablum/usermanagement/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.gablum.usermanagement.user.repository;
+
+
+import com.gablum.usermanagement.user.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+    User findUserByEmail(String email);
+}
