79857568cb89e20deccc25275dd2fe4b38bba2fa
Anurag <anuragr873@gmail.com>
2019-11-22 11:34:19 +0530

    changed bid spec file

--- gablum-webapp/src/main/resources/frontend/src/app/auction/bid-form/bid-form.component.spec.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/auction/bid-form/bid-form.component.spec.ts
@@ -1,29 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BidFormComponent } from './bid-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../../material/material.module';
-
-
-
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 describe('BidFormComponent', () => {
   let component: BidFormComponent;
   let fixture: ComponentFixture<BidFormComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ BidFormComponent ],
-      imports: [ ReactiveFormsModule, MaterialModule ]
+      imports: [ReactiveFormsModule, MaterialModule, HttpClientModule, BrowserAnimationsModule, NoopAnimationsModule],
+      providers: [
+        BrowserAnimationsModule,
+        NoopAnimationsModule
+      ]
     })
     .compileComponents();
   }));
-
   beforeEach(() => {
     fixture = TestBed.createComponent(BidFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
