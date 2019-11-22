e5cfa185c7eac003865bf0baeb8e769b0cf7b407
anup singh <anup.singh2@boeing.com>
2019-11-22 18:56:15 +0530

    New Proposal Dialog patch 3

--- gablum-webapp/src/main/resources/frontend/src/app/dashboard/dashboard.module.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/dashboard/dashboard.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DetailComponent } from './detail/detail.component';
 import { AuctionsListComponent } from './auctions-list/auctions-list.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    DetailComponent,
     AuctionsListComponent
   ],
   imports: [
