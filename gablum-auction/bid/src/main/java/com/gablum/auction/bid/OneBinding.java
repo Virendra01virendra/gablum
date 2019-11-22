3d050a85b0044533ea22bfdcfee5c5e2ac7706ff
Sudhindra <sudhindra.katre@boeing.com>
2019-11-22 17:24:38 +0530

    connected bidui to backend

--- gablum-auction/bid/src/main/java/com/gablum/auction/bid/OneBinding.java
+++ gablum-auction/bid/src/main/java/com/gablum/auction/bid/OneBinding.java
@@ -1,10 +1,10 @@
 package com.gablum.auction.bid;
-
-import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.messaging.SubscribableChannel;
-
-interface OneBinding {
-
-    @Input("chn")
-    public SubscribableChannel getChannel();
-}
+//
+//import org.springframework.cloud.stream.annotation.Input;
+//import org.springframework.messaging.SubscribableChannel;
+//
+//interface OneBinding {
+//
+//    @Input("chn")
+//    public SubscribableChannel getChannel();
+//}
