58e6aba1765ee15d5bd16bfefbab3a0b528143e9
virendra <virendra.virendra@boeing.com>
2019-11-26 14:50:20 +0530

    fixing pmd violations in timer branch

--- gablum-proposals/src/main/java/com/gablum/proposals/SchedulerApiApplication.java
+++ gablum-proposals/src/main/java/com/gablum/proposals/SchedulerApiApplication.java
@@ -3,7 +3,6 @@ package com.gablum.proposals;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import rx.Scheduler;
 
 @EnableEurekaClient
 @SpringBootApplication
