89622fc49e8303bfa144f2ceb759e5cf2d386e26
pratik ninawe <pratik.ninawe@boeing.com>
2019-11-27 11:24:21 +0530

    manage_profile_frontend

--- gablum-webapp/src/main/resources/frontend/src/app/profile/profile-page/profile-page.component.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/profile/profile-page/profile-page.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit() {
--- gablum-webapp/src/main/resources/frontend/src/app/profile/profile.module.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/profile/profile.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [ProfilePageComponent],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    FlexLayoutModule
   ]
 })
 export class ProfileModule { }
