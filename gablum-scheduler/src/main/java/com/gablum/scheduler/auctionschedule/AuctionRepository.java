d4542a4dd8d557f6ad371877231da17bf142d756
virendra <virendra.virendra@boeing.com>
2019-11-29 10:11:48 +0530

    Scheduler boilerplate

--- gablum-scheduler/src/main/java/com/gablum/scheduler/auctionschedule/AuctionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gablum.scheduler.auctionschedule;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface AuctionRepository extends MongoRepository<AuctionModel, String> {
-
-}
--- gablum-scheduler/src/main/java/com/gablum/scheduler/auctionschedule/AuctionService.java
+++
@@ -1,7 +0,0 @@
-package com.gablum.scheduler.auctionschedule;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuctionService {
-}
