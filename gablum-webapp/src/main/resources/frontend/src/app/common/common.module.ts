126cdf7392b4f98f87244307f0cfbaf457f46884
virendra <virendra.virendra@boeing.com>
2019-12-04 11:11:22 +0530

    resolved frontend bug

--- gablum-webapp/src/main/resources/frontend/src/app/common/common.module.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/common/common.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarMaterialModule } from './navbar/navbar-material.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
