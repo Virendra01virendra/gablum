8d5693a1e2ae7ee3ac5709bda319c4b7f22d893c
Abhishek <er.abhisheksingh2764@gmail.com>
2019-12-04 19:35:39 +0530

    showing dashboard after deletion of proposal card

--- gablum-webapp/src/main/resources/frontend/src/app/dashboard/new-proposal-card/new-proposal-card.component.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/dashboard/new-proposal-card/new-proposal-card.component.ts
@@ -46,6 +46,8 @@ export class NewProposalCardComponent implements OnInit {
   delete(proposal: Proposal) {
     console.log('delete function is getting called');
     this.proposalDataService.deleteProposal(proposal.proposalId, NewProposalCardComponent.messageKey, 'form-delete');
+    this.proposalDataService.getAllProposals('DashboardComponent','proposals');
     this.router.navigate(['/dashboard']);
+  
   }
 }
