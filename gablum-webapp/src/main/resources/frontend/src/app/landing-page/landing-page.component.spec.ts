a52dfefa85c229075a0a9102d4f8a246ea85333a
Anurag <anuragr873@gmail.com>
2019-12-05 20:48:07 +0530

    resolved lint errors

--- gablum-webapp/src/main/resources/frontend/src/app/landing-page/landing-page.component.spec.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/landing-page/landing-page.component.spec.ts
@@ -3,8 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LandingPageComponent } from './landing-page.component';
 import { LoginComponent } from '../login/login.component';
 import { MatToolbarModule, MatCardModule,
-  MatFormFieldModule, MatButtonModule, MatRadioModule, MatInputModule, 
-  MatDividerModule, MatListModule, MatIconModule, MatDialog, MatDialogModule } from '@angular/material';
+  MatFormFieldModule, MatButtonModule, MatRadioModule, MatInputModule,
+  MatDividerModule, MatListModule, MatIconModule, MatDialogModule } from '@angular/material';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
