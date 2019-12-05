3595f106fd19c6bc8fd373908d8cf2359e050cd5
ananyo sen <ananyo.sen@boeing.com>
2019-12-05 23:50:21 +0530

    add toolbars to few components

--- gablum-webapp/src/main/resources/frontend/src/app/contracts/contracts.module.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/contracts/contracts.module.ts
@@ -3,17 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ContractComponent } from './contract/contract.component';
 import { ContractRoutingModule } from './contract-routing.module';
 import { MatCardModule, MatButtonModule, MatListModule, MatExpansionModule } from '@angular/material';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [ContractComponent],
   imports: [
     CommonModule,
     ContractRoutingModule,
-    // MaterialModule,
-    MatCardModule,
-    MatButtonModule,
-    MatListModule,
-    MatExpansionModule
+    MaterialModule,
   ]
 })
 export class ContractsModule { }
