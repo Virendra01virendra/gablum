15f872cc1adb1ec758dfc6c4e4af59b30a5978a7
ananyo sen <ananyo.sen@boeing.com>
2019-12-08 11:53:35 +0530

    profile edit works, updates as soon as edit is made

--- gablum-webapp/src/main/resources/frontend/src/app/services/buyer-guard.service.spec.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/services/buyer-guard.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BuyerGuardService } from './buyer-guard.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('BuyerGuardService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: BuyerGuardService = TestBed.get(BuyerGuardService);
