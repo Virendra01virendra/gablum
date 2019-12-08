26de73b8b96eb7d1603cc7ca7d18d4b9e1641afa
ananyo sen <ananyo.sen@boeing.com>
2019-12-08 12:56:54 +0530

    show login error message

--- gablum-webapp/src/main/resources/frontend/src/app/services/login-data.service.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/services/login-data.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { LoginRequest } from '../interfaces/login-request';
 import { LoginToken } from '../interfaces/login-token';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { CommunicatorService } from './communicator.service';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { LoggerService } from './logger.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,8 @@ export class LoginDataService {
 
   constructor(
     private http: HttpClient,
-    private comms: CommunicatorService
+    private comms: CommunicatorService,
+    private logger: LoggerService
     ) { }
 
   login(params: LoginRequest) {
@@ -27,20 +29,30 @@ export class LoginDataService {
       JSON.stringify(params),
       httpOptions
       )
-      .pipe(
-        catchError(err => {
-          this.comms.postMessage(
-            this,
-            '@all',
-            {loginResult: {accessToken: null}});
-          return throwError(err);
-        })
-      ).subscribe(res => {
+      .subscribe(res => {
         this.comms.postMessage(
           this,
           '@all',
           {loginResult: {accessToken: res}}
         );
+      },
+      err => {
+        this.logger.log(err);
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+            this.comms.postMessage(
+              this,
+              '@all',
+              {loginResult: {accessToken: err.status}}
+            );
+          } else {
+            this.comms.postMessage(
+              this,
+              '@all',
+              {loginResult: {accessToken: 500}}
+            );
+          }
+        }
       });
   }
 
