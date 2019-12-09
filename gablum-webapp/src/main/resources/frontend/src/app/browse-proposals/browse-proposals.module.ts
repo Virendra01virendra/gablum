e42d4ef24f5b9d24453769cce96b0af602cbe2c3
ananyo sen <ananyo.sen@boeing.com>
2019-12-09 14:08:37 +0530

    remove jpg hero image

---
+++ gablum-webapp/src/main/resources/frontend/src/app/browse-proposals/browse-proposals.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BrowseProposalsRoutingModule } from './browse-proposals-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowseProposalsRoutingModule
+  ]
+})
+export class BrowseProposalsModule { }
