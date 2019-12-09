19bd006086418c8120ad4704aa4481f02e246cdb
ananyo sen <ananyo.sen@boeing.com>
2019-12-09 19:01:58 +0530

    add graphs to bid

--- gablum-webapp/src/main/resources/frontend/src/app/auction/bid-card/bid-card.component.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/auction/bid-card/bid-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NewBid } from 'src/app/interfaces/newbid';
 import { MatChipsModule } from '@angular/material/chips';
+import { NgxData } from 'src/app/interfaces/ngx-data';
 
 
 @Component({
@@ -16,8 +17,15 @@ export class BidCardComponent implements OnInit {
 
   @Input() public bidDataEntity: NewBid;
 
+  public data: NgxData[];
+
 
   ngOnInit() {
+    this.data = Object.keys(this.bidDataEntity.score).map(key => {
+      // if (key !== 'total') {
+        return {name: key, value: this.bidDataEntity.score[key]};
+      // }
+    }).filter(item => item.name !== 'total');
   }
 
 }
