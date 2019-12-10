31157c6179604ffaa7021b616f4ebba7b7fc356d
virendra <virendra.virendra@boeing.com>
2019-12-10 15:38:16 +0530

    fixing pmd violations

--- gablum-scheduler/src/main/java/com/gablum/scheduler/proposalschedule/Scheduler/QuartzScheduling/SchedulerJob.java
+++ gablum-scheduler/src/main/java/com/gablum/scheduler/proposalschedule/Scheduler/QuartzScheduling/SchedulerJob.java
@@ -1,46 +1,30 @@
 package com.gablum.scheduler.proposalschedule.Scheduler.QuartzScheduling;
 
 import com.gablum.scheduler.proposalschedule.Controller.SchedulesController;
-import com.gablum.scheduler.proposalschedule.rabbit.SchedulerPublisher;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.SchedulerException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.context.ApplicationContext;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-//@RequestMapping
 @NoArgsConstructor
-//@EnableBinding(SchedulerPublisher.class)
 public class SchedulerJob implements Job {
-//    @Output("startAuction")
-//    private MessageChannel messageChannel;
-//
-//    @Autowired
-//    public SchedulerJob(SchedulerPublisher publisher) {
-//        messageChannel = publisher.getchannel();
-//    }
-//
+
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException, NullPointerException {
         try {
-            ApplicationContext applicationContext = (ApplicationContext) jobExecutionContext.getScheduler().getContext().get(SchedulerPublisher.class);
-//            messageChannel = applicationContext.getBean(SchedulerPublisher.class).getchannel();
+
             MessageChannel messageChannel = SchedulesController.messageChannel;
             Message<String> msg = MessageBuilder.withPayload("hello auction from Scheduler").build();
             messageChannel.send(msg);
             log.info("-----------------------------Hello Quartz-----------------------------");
-        } catch (SchedulerException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
