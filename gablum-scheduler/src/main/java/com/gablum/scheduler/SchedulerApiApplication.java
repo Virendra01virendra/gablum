fe791e8e486ba88b4f253ecd27be5bff03c75a71
virendra <virendra.virendra@boeing.com>
2019-12-10 15:40:30 +0530

    fixing pmd violations

--- gablum-scheduler/src/main/java/com/gablum/scheduler/SchedulerApiApplication.java
+++ gablum-scheduler/src/main/java/com/gablum/scheduler/SchedulerApiApplication.java
@@ -1,6 +1,5 @@
 package com.gablum.scheduler;
 
-import com.gablum.scheduler.proposalschedule.Scheduler.QuartzScheduling.SchedulerJob;
 import com.gablum.scheduler.proposalschedule.rabbit.SchedulerPublisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
