c3e829613e50dc64041b8874b805649b63077bf6
pratik ninawe <pratik.ninawe@boeing.com>
2019-12-11 19:58:55 +0530

    contract routing module fixed

--- gablum-webapp/src/main/resources/frontend/src/app/contracts/contract-page/contract-page.component.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/contracts/contract-page/contract-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileDataService } from 'src/app/services/profile-data.service';
+import { CommunicatorService } from 'src/app/services/communicator.service';
+import { Router } from '@angular/router';
+import { LoggerService } from 'src/app/services/logger.service';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contract-page',
@@ -7,7 +13,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContractPageComponent implements OnInit {
 
-  constructor() { }
+  public static messageKey = 'ContractPageComponent';
+
+  public isLoggedIn = false;
+  public isBuyer;
+  public isSeller;
+  public userRole = new Array();
+
+  constructor(
+    private user: ProfileDataService,
+    private comms: CommunicatorService,
+    private router: Router,
+    private logger: LoggerService,
+    private auth: AuthenticationService,
+    public http: HttpClient
+  ) {
+    this.isLoggedIn = auth.getAuthenticated();
+    if (this.isLoggedIn) {
+      this.logger.log(this, auth.getProfileData());
+  }
 
   ngOnInit() {
   }
