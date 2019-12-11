1c1010fd270731baf6498472ae15d5195a63ad0f
anup singh <anup.singh2@boeing.com>
2019-12-11 13:27:52 +0530

    invited seller button

--- gablum-webapp/src/main/resources/frontend/src/app/dashboard/sellers-list-dialog/sellers-list-dialog.component.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/dashboard/sellers-list-dialog/sellers-list-dialog.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ProposalsDataService } from 'src/app/services/proposals-data.service';
+import { CommunicatorService } from 'src/app/services/communicator.service';
+import { Profile } from 'src/app/interfaces/profile';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-sellers-list-dialog',
@@ -11,13 +14,39 @@ export class SellersListDialogComponent implements OnInit {
 
   public static messageKey = 'sellers-list-dialog-component';
   disabled = false;
+  buttonClicked =  false;
+  public isLoggedIn = false;
+  public isBuyer = false;
+  public isSeller = false;
+  public profile: Profile;
+  alreadyRegistered: boolean;
+  public userEmail = '';
 
-  constructor( @Inject(MAT_DIALOG_DATA) public data: any, private proposalService: ProposalsDataService) { }
+  constructor( @Inject(MAT_DIALOG_DATA) public data: any, private proposalService: ProposalsDataService, comms: CommunicatorService,
+               private auth: AuthenticationService) {
+    comms.getMessages().subscribe(msg => {
+      if (msg.dest === SellersListDialogComponent.messageKey || msg.dest === '@all') {
+        const Data = msg.data;
+
+        if ('authChanged' in Data) {
+          this.isLoggedIn = auth.getAuthenticated();
+          this.profile = auth.getProfileData();
+          this.isBuyer = auth.isBuyer();
+          this.isSeller = auth.isSeller();
+          this.userEmail = this.profile.email;
+        }
+
+        if ('invite-seller' in Data) {
+          this.buttonClicked = true;
+        }
+    }
+  });
+   }
 
   ngOnInit() {
   }
 
-  onInvite() {
+  onInvite(data) {
     this.disabled = true;
     this.proposalService.postInvitedSeller(SellersListDialogComponent.messageKey, this.data, 'invite-seller');
   }
