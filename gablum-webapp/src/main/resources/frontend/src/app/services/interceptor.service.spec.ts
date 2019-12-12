18cc755763730482e800c11ac968c83094d2d4e0
ananyo sen <ananyo.sen@boeing.com>
2019-12-12 11:24:03 +0530

    tests pass now

--- gablum-webapp/src/main/resources/frontend/src/app/services/interceptor.service.spec.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/services/interceptor.service.spec.ts
@@ -2,11 +2,13 @@ import { TestBed } from '@angular/core/testing';
 
 import { InterceptorService } from './interceptor.service';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('InterceptorService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
-      HttpClientModule
+      HttpClientModule,
+      RouterTestingModule
     ]
   }));
 
