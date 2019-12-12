a699eaefc774f2e6a4883ed463ea9c5636a8785c
pratik ninawe <pratik.ninawe@boeing.com>
2019-12-12 15:07:25 +0530

    contract-detail dialog .ts

--- gablum-webapp/src/main/resources/frontend/src/app/contracts/contract-detail/contract-detail.component.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/contracts/contract-detail/contract-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { ContractDetail } from 'src/app/interfaces/contract-detail';
 import { ContractsDataService } from 'src/app/services/contracts-data.service';
 import { Auction } from 'src/app/interfaces/auction';
 import { Proposal } from 'src/app/interfaces/proposal';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-contract-detail',
@@ -11,21 +12,20 @@ import { Proposal } from 'src/app/interfaces/proposal';
   styleUrls: ['./contract-detail.component.css']
 })
 export class ContractDetailComponent implements OnInit {
+
+  public static messagekey = 'ContractDetail';
   public contract: ContractDetail;
   public auctionDetails: Auction;
   public proposal: Proposal;
   public productName: string;
   public sellerName: string;
   constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private matDialog: MatDialog,
     private route: ActivatedRoute,
     private router: Router,
     private contractService: ContractsDataService
-  ) {
-    // this.contract = this.contractService.retrieveContract();
-    // this.auctionDetails = this.contract.auctionDetails;
-    // this.proposal = this.auctionDetails.proposal;
-    // this.productName = this.proposal.productName;
-  }
+  ) { }
 
   ngOnInit() {}
 }
