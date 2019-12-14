ee81488fdb3b83d2c884c8abb075cee2c3f29c5c
Abhishek <er.abhisheksingh2764@gmail.com>
2019-12-14 19:06:28 +0530

    Resolvingangular test cases

--- gablum-webapp/src/main/resources/frontend/src/app/contracts/contract-detail/contract-detail.component.spec.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/contracts/contract-detail/contract-detail.component.spec.ts
@@ -11,29 +11,109 @@ import { Router } from '@angular/router';
 describe('ContractDetailComponent', () => {
   let component: ContractDetailComponent;
   let fixture: ComponentFixture<ContractDetailComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContractDetailComponent ],
+      declarations: [ContractDetailComponent],
       imports: [
         BrowserAnimationsModule,
         MaterialModule,
         HttpClientTestingModule,
-        RouterTestingModule],
-      providers: [Router,
+        RouterTestingModule
+      ],
+      providers: [
         {
           provide: MAT_DIALOG_DATA
         }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ContractDetailComponent);
     component = fixture.componentInstance;
+
+
+    const contractDetail = {
+      _id: '',
+      contractId: '',
+      auctionId: '',
+      bidId: '',
+      auctionDetails: {
+        auctionId: '',
+        uniqueLink: '',
+        auctionName: '',
+        proposal: {
+          proposalId: '',
+          productId: '',
+          createdBy: '',
+          updatedBy: '',
+          businessDomain: '',
+          businessSubDomain: '',
+          productName: '',
+          quantityValue: 2,
+          quantityUnit: '',
+          price: 1,
+          priceWeight: 1,
+          deliveryDate: new Date(),
+          deliveryDateWeight: 1,
+          creditPeriod: 2,
+          creditPeriodWeight: 1,
+          qualityCertification: true,
+          qualityCertificationWeight: 2,
+          methodOfSupply: true,
+          methodOfSupplyWeight: 1,
+          regStartDate: new Date(),
+          regEndDate: new Date(),
+          auctionStartDate: new Date(),
+          auctionEndDate: new Date(),
+          createdOn: new Date(),
+          updatedOn: new Date(),
+          thresholdParticipants: 3,
+          views: 2,
+          interested: 1,
+          interestedUsersEmail: [],
+          invitedUsersEmail: [],
+          productDescription: ''
+      },
+      bidDetails: {
+        bidId: '',
+    auctionId: '',
+    bid: {
+        price: 20,
+        creditPeriod: 2,
+        qaqcCertificate: true,
+        typeOfSupply: true,
+        timeOfDelivery: new Date()
+        },
+    scoreObject: {
+        total: 3,
+        deliveryScore: 3,
+        priceScore: 2,
+        creditScore: 2,
+        qaqcScore: 2,
+        typeScore: 2
+    },
+    createdBy: ''
+      },
+      buyerEmail: '',
+      buyerESign: '',
+      sellerESign: '',
+      sellerEmail: '',
+      contractStatus: '',
+      currentHash: '',
+      previousHash: '',
+      createdOn: new Date()
+    }
+  };
+
+    component.contractDetail = contractDetail;
+
+
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
