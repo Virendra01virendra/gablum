3bc6e535bd4da5a687479c18a9433136a6d82c6e
pratik ninawe <pratik.ninawe@boeing.com>
2019-12-15 15:38:10 +0530

    multilingual contracts

--- gablum-webapp/src/main/resources/frontend/src/app/contracts/contract-detail/contract-detail.component.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/contracts/contract-detail/contract-detail.component.ts
@@ -8,6 +8,8 @@ import { MAT_DIALOG_DATA, MatDialog } from '@angular/material';
 import { NewBid } from 'src/app/interfaces/newbid';
 import { ContractWithBothUser } from 'src/app/interfaces/contract-with-both-user';
 import { Profile } from 'src/app/interfaces/profile';
+import { TranslateService } from '@ngx-translate/core';
+import { IntlService } from 'src/app/services/intl.service';
 
 @Component({
   selector: 'app-contract-detail',
@@ -30,7 +32,9 @@ export class ContractDetailComponent implements OnInit {
     private matDialog: MatDialog,
     private route: ActivatedRoute,
     private router: Router,
-    private contractService: ContractsDataService
+    private contractService: ContractsDataService,
+    public translate: TranslateService,
+    public intl: IntlService
   ) {
     this.contract = this.contractWithBothUser.contract;
     this.buyer = this.contractWithBothUser.buyer;
@@ -40,6 +44,10 @@ export class ContractDetailComponent implements OnInit {
     this.productName = this.proposal.productName;
     this.bidDetails = this.contract.bidDetails;
     this.sellerName = this.bidDetails.createdBy;
+    translate.addLangs(['en', 'fr']);
+    translate.setDefaultLang('en');
+    // const browserLang = translate.getBrowserLang();
+    translate.use(intl.getLang());
   }
   ngOnInit() {}
 }
