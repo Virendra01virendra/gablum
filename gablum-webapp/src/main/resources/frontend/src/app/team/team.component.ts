650889950a45ca462eed053fdd6591384d2797d9
Anurag <anuragr873@gmail.com>
2019-12-16 10:40:29 +0530

    polished landing page

--- gablum-webapp/src/main/resources/frontend/src/app/team/team.component.ts
+++ gablum-webapp/src/main/resources/frontend/src/app/team/team.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamComponent implements OnInit {
 
-  public heros = ['ANaN_YO', 'VEERU_PA', 'SOODHIYA_BEATER', 'CAPTAIN', 'ABHIII', 'SCAM_MASTER', 'SU_DHIN_DHRAA', 'SWARTHY_PARTH'];
+  public heros = ['ANaN_YO', 'VEERU_PA', 'PRATS99', 'CAPTAIN', 'ABHI', 'SCAM_MASTER', 'SU_DHIN_DHRAA', 'SWARTHY_PARTH'];
 
   constructor() {
    }
